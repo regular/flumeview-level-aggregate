556c7f38464268e499c9cccead170eeda3cf2ac0
Jan Bölsche <regular.gonzales@googlemail.com>
2023-03-27 13:31:46 +0200

    Failed attempt to implement timeout

--- stream.js
+++ stream.js
@@ -4,25 +4,49 @@
 const pull = require('pull-stream')
 const win = require('pull-window')
 
-module.exports = function(fitsBucket, add) {
-  let current = null
-  let next = undefined
+module.exports = function(fitsBucket, add, timeout) {
+  let bucket
+  let startNew = true
+  let timer
 
   return win(startf, mapf)
 
   function startf(data, cb) {
-    if (current !== null) return 
-    current = next
+    console.log('>', data)
+    if (!startNew) return 
+    console.log('start window')
+    startNew = false
+
+    function setTimer() {
+      console.log('setTimeout')
+      if (timer) clearTimeout(timer)
+      timer = setTimeout(()=>{
+        timer = null
+        console.log('XXXXXXXX timeout', bucket)
+        //if (bucket !== null && bucket !== undefined) {
+          done(null, bucket)
+        //}
+      }, timeout)
+    }
+
+    function done(err, acc) {
+      console.log('done', acc)
+      if (timer) clearTimeout(timer)
+      timer = null
+      startNew = true
+      cb(err, acc)
+    }
+
 
     return function(end, data) {
-      if (end) return cb(end, current)
+      if (end) return done(end, bucket)
+      setTimer()
 
-      if (!current || fitsBucket(current, data)) {
-        current = add(current, data)
+      if (!bucket || fitsBucket(bucket, data)) {
+        bucket = add(bucket, data)
       } else {
-        next = add(undefined, data)
-        cb(null, current)
-        current = null
+        done(null, bucket)
+        bucket = add(undefined, data)
       }
     }
   }
--- test-stream.js
+++ test-stream.js
@@ -2,27 +2,10 @@ const test = require('tape')
 const pull = require('pull-stream')
 const Stream = require('./stream')
 
-test('aggregates values', t=>{
-
-  function bucket(n) {
-    return (n / 10) << 0
-  }
-
-  function add(b, value) {
-    b = b || {id: bucket(value), l: [], sum: 0}
-    b.sum += value
-    b.l.push(value)
-    return b
-  }
-
-  // empty buckets fit all
-  function fitsBucket(b,i) {
-    return bucket(i) == b.id
-  }
-
+test.skip('aggregates values', t=>{
   pull(
     pull.values([1,2,10,11,20,25]),
-    Stream(fitsBucket, add),
+    Stream(fitsBucket, add, 1000),
     pull.collect( (err, data)=>{
       console.log(data)
       t.deepEqual(data, [
@@ -34,3 +17,59 @@ test('aggregates values', t=>{
     })
   )
 })
+
+test('dont stall', t=>{
+  pull(
+    timedSource([
+      [0, 1],
+      [0, 2],
+      [20000, 3],
+      [0, 10],
+      [0, 11],
+      [0, 20],
+      [2000, 25],
+    ]),
+    Stream(fitsBucket, add, 1000),
+    pull.through(console.log),
+    pull.collect( (err, data)=>{
+      //console.log(data)
+      t.deepEqual(data, [
+        { sum: 3, id: 0, l: [ 1, 2 ] },
+        { sum: 6, id: 0, l: [ 1, 2, 3 ] },
+        { sum: 21, id: 1, l: [ 10, 11 ] },
+        { sum: 45, id: 2, l: [ 20, 25 ] }
+      ])
+      t.end()
+    })
+  )
+})
+
+
+// - - - -
+
+function bucket(n) {
+  return (n / 10) << 0
+}
+
+function add(b, value) {
+  b = b || {id: bucket(value), l: [], sum: 0}
+  b.sum += value
+  b.l.push(value)
+  return b
+}
+
+function fitsBucket(b,i) {
+  return bucket(i) == b.id
+}
+
+function timedSource(data) {
+  return pull(
+    pull.values(data),
+    pull.asyncMap(function(item, cb) {
+      setTimeout(function() {
+        cb(null, item[1])
+      }, item[0]);
+    })
+  )
+}
+
