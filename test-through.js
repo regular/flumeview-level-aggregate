30dff214ac07d9e9a8eb2e5440fe0eecd72398e7
Jan Bölsche <regular.gonzales@googlemail.com>
2023-04-14 11:56:11 +0200

    Refactor through stream

---
+++ test-through.js
@@ -0,0 +1,153 @@
+//jshint esversion:11
+//jshint -W033
+const test = require('tape')
+const pull = require('pull-stream')
+const Stream = require('./through')
+
+test('aggregates values', t=>{
+  let seq = 0
+  pull(
+    pull.values([1,2,10,11,20,25]),
+    pull.map(value=>{return {value, seq: seq++}}),
+    Stream(fitsBucket, add),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {seq: 1, key: 0, value: { sum: 3, l: [ 1, 2 ] }},
+        {seq: 3, key: 1, value: { sum: 21, l: [ 10, 11 ] }},
+        {seq: 5, key: 2, value: { sum: 45, l: [ 20, 25 ] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+test('filter', t=>{
+  let seq = 0
+  pull(
+    pull.values([1,2,4, 10,11,12, 20,22,25]),
+    pull.map(value=>{return {value, seq: seq++}}),
+    Stream(fitsBucket, add, {filter: n=>n%2==0}),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {key: 0, seq: 2, value: { sum: 6, l: [ 2, 4 ] }},
+        {key: 1, seq: 5, value: { sum: 22, l: [ 10, 12 ] }},
+        {key: 2, seq: 8, value: { sum: 42, l: [ 20, 22 ] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+test('initial', t=>{
+  pull(
+    pull.values([{seq: 1, value: 10}]),
+    Stream(fitsBucket, add, {
+      initial: {
+        key: 1, seq: 0, value: { l:[], sum: 100}
+      }
+    }),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        { key: 1, seq: 1, value: { sum: 110, l: [ 10 ] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+test('initial, then end', t=>{
+  pull(
+    pull.values([]),
+    Stream(fitsBucket, add, {
+      initial: {
+        key: 1, seq: 100, value: { l:[], sum: 100}
+      }
+    }),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        { key: 1, seq: 100, value: { sum: 100, l: [] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+test('dont stall', t=>{
+  let seq = 0
+  pull(
+    timedSource([
+      [0, 1],
+      [0, 2],
+      [200, 3],
+      [0, 10],
+      [0, 11],
+      [0, 20],
+      [200, 25],
+    ]),
+    pull.map(value=>{return {value, seq: seq++}}),
+    Stream(fitsBucket, add, {timeout: 100}),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {key: 0, seq: 1, value: { sum: 3, l: [ 1, 2 ] }},
+        {key: 0, seq: 2, value: { sum: 6, l: [ 1, 2, 3 ] }},
+        {key: 1, seq: 4, value: { sum: 21, l: [ 10, 11 ] }},
+        {key: 2, seq: 5, value: { sum: 20, l: [ 20 ] }},
+        {key: 2, seq: 6, value: { sum: 45, l: [ 20, 25 ] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+test('max_age', t=>{
+  let seq = 0
+  pull(
+    pull.values([1,2,4, 10,11,12]),
+    pull.map(value=>{return {value, seq: seq++}}),
+    Stream(fitsBucket, add, {max_age: 2}),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {key: 0, seq: 1, value: { sum: 3, l: [ 1, 2 ] }},
+        {key: 0, seq: 2, value: { sum: 7, l: [ 1, 2, 4 ] }},
+        {key: 1, seq: 4, value: { sum: 21, l: [ 10, 11 ] }},
+        {key: 1, seq: 5, value: { sum: 33, l: [ 10, 11, 12 ] }}
+      ])
+      t.end()
+    })
+  )
+})
+
+
+// - - - -
+
+function bucketKey(n) {
+  return (n / 10) << 0
+}
+
+function add(b, n) {
+  b = b || {
+    key: bucketKey(n),
+    value: {
+      l: [], sum: 0
+    }
+  }
+  b.value.sum += n
+  b.value.l = b.value.l.concat([n])
+  return b
+}
+
+function fitsBucket({key, value}, n) {
+  return bucketKey(n) == key
+}
+
+function timedSource(data) {
+  return pull(
+    pull.values(data),
+    pull.asyncMap(function(item, cb) {
+      setTimeout(function() {
+        cb(null, item[1])
+      }, item[0]);
+    })
+  )
+}
+
---
+++ through.js
@@ -0,0 +1,93 @@
+//jshint -W033
+//jshint -W018
+//jshint  esversion: 11
+const pull = require('pull-stream')
+const copy = require('deep-copy')
+
+module.exports = function(fitsBucket, add, opts) {
+  opts = opts || {}
+  let {timeout, initial, filter, max_age} = opts
+  filter = filter || (x=>true)
+  if (max_age == undefined) max_age = 512
+
+  let end, timer, bucket = initial, reading
+  let rec_cnt = 0
+  let seq = initial && initial.seq
+  if (seq == undefined) seq = -1
+  const cbs = []
+  const buff = []
+
+  return function (read) {
+
+    return function (abort, cb) {
+      cbs.push(cb)
+
+      if (buff.length) {
+        cb = cbs.shift()
+        const [err, acc, seq] = buff.shift()
+        if (err) return cb(err)
+        return cb(null, {key: acc && acc.key, value: acc && acc.value, seq})
+      }
+      if (end) return flush(end)
+      if (reading) return
+
+      function flush(err, acc) {
+        buff.push([err, acc, seq])
+        cb = cbs.shift()
+        if (cb) {
+          if (timer) clearTimeout(timer)
+          timer = null
+          const [err, acc, seq] = buff.shift()
+          if (err) return cb(err)
+          return cb(null, {key: acc && acc.key, value: acc && acc.value, seq})
+        }
+      }
+
+      function setTimer() {
+        if (timer) clearTimeout(timer)
+        if (!timeout) return
+        timer = setTimeout(()=>{
+          timer = null
+          if (bucket !== null && bucket !== undefined) {
+            flush(null, copy(bucket))
+          }
+        }, timeout)
+      }
+
+      setTimer()
+
+      function slurp() {
+        reading = true
+        read(abort, (err, data) =>{
+          reading = false
+          if (err) {
+            end = err
+            if (bucket) flush(null, bucket)
+            else flush(err)
+            return
+          }
+
+          if (!filter(data.value)) {
+            seq = data.seq
+            return slurp()
+          }
+
+          if (!bucket || fitsBucket(bucket, data.value)) {
+            seq = data.seq
+            bucket = add(bucket, data.value)
+            if (max_age && ++rec_cnt >= max_age) {
+              rec_cnt = 0
+              flush(null, copy(bucket))
+            }
+            return slurp()
+          } else {
+            flush(null, bucket)
+            seq = data.seq
+            bucket = add(undefined, data.value)
+          }
+        })
+      }
+      slurp()
+    }
+  }
+}
