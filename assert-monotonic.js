b56be3fb4c05f9b3338cdb24bcca748d9f71f71b
Jan Bölsche <regular.gonzales@googlemail.com>
2024-06-10 11:06:48 +0200

    Better diagnostic output for assert-monotonic

--- assert-monotonic.js
+++ assert-monotonic.js
@@ -2,32 +2,42 @@
 
 const pull = require('pull-stream')
 const {inspect} = require('util')
+const copy = require('deep-copy')
 
 module.exports = function(f, name, cb) {
   if (!f) return pull.through(x=>{}) 
   
   let last = null
   let sign = null
+  let prevValue = null
 
-  return pull.through( (value)=>{
-    const curr = f(value)
-    if (curr == undefined) return
-    if (last == null) {
+  return pull(
+    pull.through( value=>{
+      const curr = f(value)
+      if (curr == undefined) return
+      if (last == null) {
+        last = curr
+        return
+      }
+      const delta = curr - last
+      const _last = last
       last = curr
-      return
-    }
-    const delta = curr - last
-    last = curr
-    if (delta == 0) return
-    const currSign = Math.sign(delta)
-    if (sign == null) {
-      sign = currSign
-      return
-    }
-    if (currSign !== sign) {
-      const msg = (`MONOTONIC ASSERTION ${name || '[unnamed]'} FAILED at value: ${inspect(value)}`)
-      if (cb) cb(msg)
-    }
-  })
+      if (delta == 0) return
+      const currSign = Math.sign(delta)
+      if (sign == null) {
+        sign = currSign
+        return
+      }
+      if (currSign !== sign) {
+        const msg = `MONOTONIC ASSERTION ${name || '[unnamed]'} FAILED (${_last} followed by ${curr}, delta = ${delta}, expected sign: ${sign}) at values:\nprev = ${inspect(prevValue)}\ncurr = ${inspect(value)}`
+        if (cb) cb(msg)
+      }
+    }),
+    pull.through( value=>{
+      if (f(value) !== undefined) {
+        prevValue = copy(value)
+      }
+    })
+  )
 }
 
